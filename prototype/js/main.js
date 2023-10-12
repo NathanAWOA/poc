6cc29b7bf174201964d4a7b66a081a1f7cff241d
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-10-12 13:19:12 -0300

    pausa para o rango

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,5 +1,9 @@
 const card = document.querySelector(".card-inner")
+const finish = document.querySelector(".finish")
+const f = document.querySelector(".btn-f")
+const gg = document.querySelector(".btn-gg")
 
 card.addEventListener("click", function(e) {
     card.classList.toggle('is-flipped')
+    finish.classList.toggle('is-clicked')
 })
